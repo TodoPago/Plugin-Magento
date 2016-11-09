76cf8ce9f34bc8cde7291121c004706d997ee757
Matias Montes <matias@semexpert.com.ar>
2016-11-09 00:07:47 -0300

    Fix deun typo

--- app/code/community/Todopago/Modulodepago2/controllers/PaymentController.php
+++ app/code/community/Todopago/Modulodepago2/controllers/PaymentController.php
@@ -40,7 +40,7 @@ class Todopago_Modulodepago2_PaymentController extends Mage_Core_Controller_Fron
         $order = Mage::getSingleton('sales/order')
         ->loadByIncrementId($id);
 
-        $message = "Actualizaco automáticamente por Todo Pago";
+        $message = "Actualizado automáticamente por Todo Pago";
         $status = Mage::getStoreConfig('payment/modulodepago2/order_status');
         if(empty($status)) $status = Mage::getStoreConfig('payment/todopago_avanzada/order_status');
         $order->setState("new", $status, $message);
